c747d3bc7a9eec7d5b5a475437d58f8f2d9f0057
kevin Mendez <kmendezp@unal.edu.co>
2018-09-04 09:44:56 -0500

    diagrama y pequeños cambios

---
+++ src/test/java/helpers/TestApi.java
@@ -0,0 +1,14 @@
+package helpers;
+
+import cucumber.api.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features = {"src/test/resources/features/songsByGenre.feature",
+        "src/test/resources/features/songsById.feature",
+        "src/test/resources/features/storedInstances.feature",
+        "src/test/resources/features/topFiveSongs.feature"},
+        glue = "steps")
+public class TestApi {
+}
--- src/test/java/helpers/TestSong.java
+++ src/test/java/helpers/TestSong.java
@@ -5,7 +5,9 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features = {"src/test/resources/features/createSong.feature", "src/test/resources/features/updateSong.feature", "src/test/resources/features/deleteSong.feature"},
+@CucumberOptions(features = {"src/test/resources/features/createSong.feature",
+                            "src/test/resources/features/updateSong.feature",
+                            "src/test/resources/features/deleteSong.feature"},
                     glue = "steps")
 public class TestSong {
 }
